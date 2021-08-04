49ec02008c08722511eb85906e650114b02ab4e4
Hayley Witherell <hayleywitherell@gmail.com>
2021-08-03 21:11:14 -0600

    Start constructor

--- src/Trip.js
+++ src/Trip.js
@@ -1,5 +1,14 @@
 class Trip {
-  constructor() {
+  constructor(tripsData, destinationsData) {
+    this.id = tripsData.id,
+    this.userID = tripsData.userID,
+    this.destinationID = tripsData.destinationID,
+    this.travelers = tripsData.travelers,
+    this.date = tripsData.date,
+    this.duration = tripsData.duration,
+    this.status = tripsData.status,
+    this.suggestedActivities = tripsData.suggestedActivities,
+    this.destinationsData = destinationsData
   }
 }
 
