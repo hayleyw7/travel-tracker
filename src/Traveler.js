0303c84f1b02317e723361e22e8cf2c03994f0eb
Hayley Witherell <hayleywitherell@gmail.com>
2021-08-06 15:10:36 -0600

    Make methods

--- src/Traveler.js
+++ src/Traveler.js
@@ -1,12 +1,11 @@
 class Traveler {
 
-  constructor(data, trips) {
-
-    this.name = data.name;
+  constructor(data, trips, destinations) {
     this.id = data.id;
+    this.name = data.name;
+    this.trips = trips;
+    this.destinations = destinations;
     this.travelerType = data.travelerType;
-    this.trips = trips.filter(trip => trip.userID === this.id);
-
   }
 
   getName() {
@@ -25,6 +24,25 @@ class Traveler {
     return this.trips;
   }
 
+  getTotalSpent() {
+
+    return this.trips.reduce((sum, trip) => {
+
+      const destination = this.getDestination(trip);
+
+      const flightCost = trip.travelers * destination.estimatedFlightCostPerPerson;
+
+      const lodgingCost = trip.duration * destination.estimatedLodgingCostPerDay;
+
+      sum += flightCost + lodgingCost;
+
+      return sum;
+    }, 0);
+  }
+
+  getDestination(trip) {
+    return this.destinations.find(destination => destination.id === trip.destinationID);
+  }
 }
 
 export default Traveler
