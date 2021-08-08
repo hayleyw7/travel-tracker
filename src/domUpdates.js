94eb570a5371e87027d8c27cf0c8c5a5106ab773
Hayley Witherell <hayleywitherell@gmail.com>
2021-08-07 20:13:47 -0600

    Change ; to ,

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -11,29 +11,29 @@ const dom = {
   },
 
   // NAVBAR
-  navBarLinksSection: document.getElementById('navBarLinksSection');
-  navBarYourTripsBtn: document.getElementById('navBarYourTripsBtn');
-  navBarTripPlannerBtn: document.getElementById('navBarTripPlannerBtn');
+  navBarLinksSection: document.getElementById('navBarLinksSection'),
+  navBarYourTripsBtn: document.getElementById('navBarYourTripsBtn'),
+  navBarTripPlannerBtn: document.getElementById('navBarTripPlannerBtn'),
 
   // LOGIN PAGE
-  loginPage: document.getElementById('loginPage');
-  loginFormUsername: document.getElementById('loginFormUsername');
-  loginFormPassword: document.getElementById('loginFormPassword');
-  loginFormSubmitBtn: document.getElementById('loginFormSubmitBtn');
-  enterYourPassToPlan: document.getElementById('enterYourPassToPlan');
+  loginPage: document.getElementById('loginPage'),
+  loginFormUsername: document.getElementById('loginFormUsername'),
+  loginFormPassword: document.getElementById('loginFormPassword'),
+  loginFormSubmitBtn: document.getElementById('loginFormSubmitBtn'),
+  enterYourPassToPlan: document.getElementById('enterYourPassToPlan'),
 
   // YOUR TRIPS DASHBOARD PAGE
-  yourTripsDashboardPage: document.getElementById('yourTripsDashboardPage');
+  yourTripsDashboardPage: document.getElementById('yourTripsDashboardPage'),
 
   // WANNA JET PAGE
-  wannaJetPage: document.getElementById('wannaJetPage');
-  jetFormDate: document.getElementById('jetFormDate');
-  jetFormDuration: document.getElementById('jetFormDuration');
-  jetFormHumans: document.getElementById('jetFormHumans');
-  jetFormDestination: document.getElementById('jetFormDestination');
-  jetFormSubmitBtn: document.getElementById('jetFormSubmitBtn');
-  estimatedCostHTML: document.getElementById('estimatedCostHTML');
-  letsJetBtn: document.getElementById('letsJetBtn');
+  wannaJetPage: document.getElementById('wannaJetPage'),
+  jetFormDate: document.getElementById('jetFormDate'),
+  jetFormDuration: document.getElementById('jetFormDuration'),
+  jetFormHumans: document.getElementById('jetFormHumans'),
+  jetFormDestination: document.getElementById('jetFormDestination'),
+  jetFormSubmitBtn: document.getElementById('jetFormSubmitBtn'),
+  estimatedCostHTML: document.getElementById('estimatedCostHTML'),
+  letsJetBtn: document.getElementById('letsJetBtn'),
 }
 
 export default dom;
