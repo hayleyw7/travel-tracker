2f87e1e03f9a06048e8726070c2dff38ea6582f0
Hayley Witherell <hayleywitherell@gmail.com>
2021-08-10 16:00:21 -0600

    Add new test

--- test/Trip-test.js
+++ test/Trip-test.js
@@ -1,7 +1,3 @@
-
-
-
-
 import chai from 'chai';
 import Traveler from '../src/Traveler.js';
 import Trip from '../src/Trip.js';
@@ -10,6 +6,7 @@ const expect = chai.expect;
 
 const travelers = require('../src/travelers');
 const trips = require('../src/trips');
+const destinations = require('../src/trips');
 
 const data = travelers.find(traveler => traveler.id === 1)
 
@@ -20,15 +17,14 @@ const bob = new Traveler(data, trips);
 
 
 describe('Trip', () => {
-
   it('is a function', function() {
     expect(Trip).to.be.a('function');
   });
 
-  // it('should be an instance of Trip', function() {
-  //   const trip = new Trip(tripsData.trips[0], destinationsData.destinations[0]);
+  it('should be an instance of Trip', function() {
+    let trip = new Trip(trips);
 
-  //   expect(trip).to.be.an.instanceof(Trip);
-  // });
+    expect(trip).to.be.an.instanceof(Trip);
+  });
 
 });
